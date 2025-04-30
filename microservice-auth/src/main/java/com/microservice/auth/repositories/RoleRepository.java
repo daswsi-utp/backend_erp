ab9632b8eac2fe84839b4b5d9fe96ede34c331df
EstefaniDavila <estefani.davila@tecsup.edu.pe>
2025-04-30 15:34:59 -0500

    role repository

--- microservice-auth/src/main/java/com/microservice/auth/repositories/RoleRepository.java
+++ microservice-auth/src/main/java/com/microservice/auth/repositories/RoleRepository.java
@@ -1,5 +1,9 @@
 package com.microservice.auth.repositories;
 
-public interface RoleRepository {
+import com.microservice.auth.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
