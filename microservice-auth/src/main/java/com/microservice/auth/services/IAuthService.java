f6d684faac0cd8135d1c166afee74f8912a76ea9
EstefaniDavila <estefani.davila@tecsup.edu.pe>
2025-04-30 15:42:41 -0500

    interfaz service auth

--- microservice-auth/src/main/java/com/microservice/auth/services/IAuthService.java
+++ microservice-auth/src/main/java/com/microservice/auth/services/IAuthService.java
@@ -1,5 +1,11 @@
 package com.microservice.auth.services;
 
-public interface IAuthService {
+import com.microservice.auth.dto.AuthResponse;
+import com.microservice.auth.dto.LoginRequest;
+import com.microservice.auth.dto.RegisterRequest;
+import com.microservice.auth.entities.User;
 
+public interface IAuthService {
+    AuthResponse login(LoginRequest request);
+    User register(RegisterRequest request);
 }
