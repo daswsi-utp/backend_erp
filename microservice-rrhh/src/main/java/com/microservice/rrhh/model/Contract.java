b2d72eaf7eebe6904de4c6455c5992d3665394f4
Yamada-decs <u22100700@utp.edu.pe>
2025-05-04 21:15:56 -0500

    Contract entity creation

---
+++ microservice-rrhh/src/main/java/com/microservice/rrhh/model/Contract.java
@@ -0,0 +1,45 @@
+package com.microservice.rrhh.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "contract")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contract {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "employee_id")
+	private Employee employee;
+	
+	@Column(name = "type")
+	private ContractType type;
+	
+	@Column(name = "startDate")
+	private LocalDate startDante;
+	
+	@Column(name = "endDate")
+	private LocalDate endDate;
+	
+	@Column(name = "state")
+	private ContractState state;
+	
+	@Column(name = "key")
+	private String key;
+	
+	@Column(name = "url")
+	private String url;
+}
