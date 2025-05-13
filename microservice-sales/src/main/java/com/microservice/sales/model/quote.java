e0c29d4c849a560d5f7d121881b5eada48e3da72
egusquiza31 <egusquiza.31@hotmail.com>
2025-05-13 10:33:53 -0500

    creating database columns

--- microservice-sales/src/main/java/com/microservice/sales/model/quote.java
+++ microservice-sales/src/main/java/com/microservice/sales/model/quote.java
@@ -2,18 +2,51 @@ package com.microservice.sales.model;
 
 import java.security.Timestamp;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="quote")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class quote {
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_quotes")
 	private Long id_quotes;
+	
+	@Column(name = "issueDate")
 	private Timestamp issueDate;
+	
+	@Column(name = "expirationDate")
 	private Timestamp expirationDate;
+	
+	@Column(name = "state")
 	private String state;
+	
+	@Column(name = "id_client")
 	private Long id_client;
+	
+	@Column(name = "id_employee")
 	private Long id_employee;
-	private String type_payment;
-	private String observations;
+	
+	@Column(name = "typePayment")
+	private String typePayment;
+	
+	@Column(name = "observation")
+	private String observation;
 	
 	
-	
-
 }
