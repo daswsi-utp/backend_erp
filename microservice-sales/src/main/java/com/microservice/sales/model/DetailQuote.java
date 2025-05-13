42705a2dba702bbbd75d45b1bf87e464d611fd0c
egusquiza31 <egusquiza.31@hotmail.com>
2025-05-13 12:45:57 -0500

    Creating the table and attributes of the quote detail class

--- microservice-sales/src/main/java/com/microservice/sales/model/DetailQuote.java
+++ microservice-sales/src/main/java/com/microservice/sales/model/DetailQuote.java
@@ -1,14 +1,39 @@
 package com.microservice.sales.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Table(name = "detail_quote")
 public class DetailQuote {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_detailQuote")
 	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "quotes_id", nullable = false)
 	private quote quoteId;
+	
+	@Column(name = "id_product")
 	private Long productId;
+	
+	@Column(name = "amount") //cantidad
 	private Integer amount;
+	
+	@Column(name = "discount")//descuento
 	private Integer discount;
+	
+	@Column(name = "tax")//impuesto
 	private Integer tax;
+	
+	@Column(name = "total")
 	private Integer total;
 	
-
+	
 }
