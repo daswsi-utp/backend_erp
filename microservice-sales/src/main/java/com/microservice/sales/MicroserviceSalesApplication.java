7d975be1d6f64051054bb668e438e9c1dfed5fb6
egusquiza31 <egusquiza.31@hotmail.com>
2025-05-13 10:25:59 -0500

    attributes of the quotation class

--- microservice-sales/src/main/java/com/microservice/sales/MicroserviceSalesApplication.java
+++ microservice-sales/src/main/java/com/microservice/sales/MicroserviceSalesApplication.java
@@ -10,6 +10,8 @@ public class MicroserviceSalesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceSalesApplication.class, args);
+		
+		
 	}
 
 }
---
+++ microservice-sales/src/main/java/com/microservice/sales/model/quote.java
@@ -0,0 +1,19 @@
+package com.microservice.sales.model;
+
+import java.security.Timestamp;
+
+public class quote {
+	
+	private Long id_quotes;
+	private Timestamp issueDate;
+	private Timestamp expirationDate;
+	private String state;
+	private Long id_client;
+	private Long id_employee;
+	private String type_payment;
+	private String observations;
+	
+	
+	
+
+}
