207bb99ae9b32e256b5175d2c3cb37bc60c66c57
egusquiza31 <egusquiza.31@hotmail.com>
2025-05-13 11:51:48 -0500

    creation of the enum state class

---
+++ microservice-sales/src/main/java/com/microservice/sales/model/DetailQuote.java
@@ -0,0 +1,14 @@
+package com.microservice.sales.model;
+
+public class DetailQuote {
+	
+	private Long id;
+	private quote quoteId;
+	private Long productId;
+	private Integer amount;
+	private Integer discount;
+	private Integer tax;
+	private Integer total;
+	
+
+}
