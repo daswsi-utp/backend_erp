a35eb48450126a6c1a43a7a8fdb37d24e070f7ab
egusquiza31 <egusquiza.31@hotmail.com>
2025-05-14 11:35:07 -0500

    Implement CRUD operations for the Quote entity

--- microservice-sales/src/main/java/com/microservice/sales/controller/QuoteController.java
+++ microservice-sales/src/main/java/com/microservice/sales/controller/QuoteController.java
@@ -10,6 +10,14 @@ import com.microservice.sales.repository.QuoteRepository;
 
 public class QuoteController {
 	
-
+	@Autowired
+	private QuoteRepository quoteRepository;
+	
+	public List<quote> getQuotes(){return quoteRepository.findAll();}
+	public Optional<quote> getDQuotesById(Long id) {return quoteRepository.findById(id);}
+	public quote createQuote(quote quoten) {return quoteRepository.save(quoten);}
+	public quote updateQuote(quote quoten) {return quoteRepository.save(quoten);}
+	public void deleteQuote(Long id) {quoteRepository.deleteById(id);}
+	
 	
 }
