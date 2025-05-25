d7a04eb71b9c49405f95c5a88124f0167410f52f
PinwinoVolador <froddo32@outlook.com>
2025-05-25 13:44:20 -0500

    creating entitie task

---
+++ microservice-plannig/src/main/java/com/microservice/planning/entities/Task.java
@@ -0,0 +1,41 @@
+package com.microservice.planning.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "task")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "task_id")
+	private int task_id;
+	
+	@Column(name = "task_name")
+	private String task_name;
+	
+	@Column(name = "task_description")
+	private String task_description;
+	
+	@Column(name = "task_start_date")
+	private Date task_start_date;
+	
+	@Column(name = "task_end_date")
+	private Date task_end_date;
+	
+}
