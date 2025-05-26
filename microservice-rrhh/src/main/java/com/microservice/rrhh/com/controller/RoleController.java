97508d269708b2b966dd8866dce8bb41f6830d38
Yamada-decs <u22100700@utp.edu.pe>
2025-05-25 20:02:03 -0500

    Generate api for recovery role by id

--- microservice-rrhh/src/main/java/com/microservice/rrhh/com/controller/RoleController.java
+++ microservice-rrhh/src/main/java/com/microservice/rrhh/com/controller/RoleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class RoleController {
 	@GetMapping
 	public ResponseEntity<List<Role>> getRoles(){return ResponseEntity.ok(roleService.getRoles());}
 	
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<Role> getRoleById(@PathVariable Long id){
+		return roleService.getRoleById(id)
+				.map(ResponseEntity::ok)
+				.orElseGet(()->ResponseEntity.notFound().build());
+	}
 	
 }
--- microservice-rrhh/src/main/java/com/microservice/rrhh/service/RoleService.java
+++ microservice-rrhh/src/main/java/com/microservice/rrhh/service/RoleService.java
@@ -1,6 +1,7 @@
 package com.microservice.rrhh.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,9 @@ public class RoleService {
 	private RoleRepository roleRepository;
 	
 	public List<Role> getRoles(){return roleRepository.findAll();}
+	
+	public Optional<Role> getRoleById(Long id){return roleRepository.findById(id);}
+	
+	
 	
 }
