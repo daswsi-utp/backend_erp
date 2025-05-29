35e0480f4f007c10e9b3cd2637d36aab1b6d8e42
PinwinoVolador <froddo32@outlook.com>
2025-05-28 22:30:09 -0500

    creating planParticipantId class to map composite primary key

---
+++ microservice-plannig/src/main/java/com/microservice/planning/entities/PlanParticipantId.java
@@ -0,0 +1,26 @@
+package com.microservice.planning.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class PlanParticipantId implements Serializable {
+
+    @Column(name = "plan_id")
+    private int planId;
+
+    @Column(name = "participant_id")
+    private int participantId;
+}
