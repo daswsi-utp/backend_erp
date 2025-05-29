cc7d6f881ebbbd5162a54fdea713f527b07548ef
PinwinoVolador <froddo32@outlook.com>
2025-05-28 22:42:34 -0500

    creating repository for planParticipantTask

---
+++ microservice-plannig/src/main/java/com/microservice/planning/repositories/PlanParticipantTaskRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.planning.repositories;
+
+import com.microservice.planning.entities.PlanParticipantTask;
+import com.microservice.planning.entities.PlanParticipantTaskId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanParticipantTaskRepository extends JpaRepository<PlanParticipantTask, PlanParticipantTaskId> {
+	
+}
