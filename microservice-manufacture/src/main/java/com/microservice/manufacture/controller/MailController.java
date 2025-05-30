9582cab999756f4f57a5d51f3bfd059e19bb3c6e
77Elvis77 <elvis.soto.luque@gmail.com>
2025-05-30 14:01:43 -0500

    add controller for recovery mails

--- microservice-manufacture/src/main/java/com/microservice/manufacture/controller/MailController.java
+++ microservice-manufacture/src/main/java/com/microservice/manufacture/controller/MailController.java
@@ -1,6 +1,10 @@
 package com.microservice.manufacture.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,4 +25,8 @@ public class MailController {
 		emailService.sendHTMLEmail(mail);
 	}
 	
+	@GetMapping
+	public ResponseEntity<List<Mail>> getMails(){
+		return ResponseEntity.ok(emailService.getMails());
+	}
 }
