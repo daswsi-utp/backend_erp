4854a1b7ed3ca5411688262c1eb96377bdb0631e
77Elvis77 <elvis.soto.luque@gmail.com>
2025-05-30 14:00:12 -0500

    add controller for create mail

--- microservice-manufacture/src/main/java/com/microservice/manufacture/controller/MailController.java
+++ microservice-manufacture/src/main/java/com/microservice/manufacture/controller/MailController.java
@@ -1,7 +1,24 @@
 package com.microservice.manufacture.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.manufacture.model.Mail;
+import com.microservice.manufacture.service.EmailService;
+
+@RestController
+@RequestMapping("/api/v1/manufacture/mail")
 public class MailController {
 	
+	@Autowired
+	private EmailService emailService;
 	
+	@PostMapping
+	public void sendHTMLEmail(@RequestBody Mail mail) {
+		emailService.sendHTMLEmail(mail);
+	}
 	
 }
