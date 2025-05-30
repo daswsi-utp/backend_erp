825c901eeef289e9034bc333c0ef1856641d7fb4
77Elvis77 <elvis.soto.luque@gmail.com>
2025-05-30 14:10:09 -0500

    impl creation for email service

---
+++ microservice-manufacture/src/main/java/com/microservice/manufacture/service/impl/EmailServiceImpl.java
@@ -0,0 +1,46 @@
+package com.microservice.manufacture.service.impl;
+
+import java.util.List;
+
+import com.microservice.manufacture.model.Mail;
+import com.microservice.manufacture.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+
+	@Override
+	public void sendHTMLEmail(Mail mail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendSimpleEmail(Mail mail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendEmailWithThymeLeaf(Mail mail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendEmailWithAttachment(Mail mail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Mail saveEmail(Mail mail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Mail> getMails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
