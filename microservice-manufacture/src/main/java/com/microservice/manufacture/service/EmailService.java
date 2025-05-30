ce9924e36f3cd786e3823544b1bc128e50324ef1
77Elvis77 <elvis.soto.luque@gmail.com>
2025-05-30 13:54:30 -0500

    add service for recovery emails

--- microservice-manufacture/src/main/java/com/microservice/manufacture/service/EmailService.java
+++ microservice-manufacture/src/main/java/com/microservice/manufacture/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.microservice.manufacture.service;
 
+import java.util.List;
+
 import com.microservice.manufacture.model.Mail;
 
 public interface EmailService {
@@ -11,4 +13,8 @@ public interface EmailService {
 	void sendEmailWithThymeLeaf(Mail mail);
 	
 	void sendEmailWithAttachment(Mail mail);
+	
+	Mail saveEmail(Mail mail);
+	
+	List<Mail> getMails();
 }
