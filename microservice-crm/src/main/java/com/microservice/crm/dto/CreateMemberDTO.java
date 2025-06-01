28bbb046dcdc7dc3b6d1e435fa102e5c42d76a7b
EstefaniDavila <estefani.davila@tecsup.edu.pe>
2025-06-01 10:01:45 -0500

    create memberdto

---
+++ microservice-crm/src/main/java/com/microservice/crm/dto/CreateMemberDTO.java
@@ -0,0 +1,15 @@
+package com.microservice.crm.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateMemberDTO {
+    private Long employeeId;
+    private String fullName;
+    private String crmRole;
+    private Long teamId;
+    private Integer status;
+}
