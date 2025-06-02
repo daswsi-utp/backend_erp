d35bead01fde0ea304bc125c3110b5a5f62311a4
SebastianT28 <sebasticlavilca@gmail.com>
2025-06-02 18:01:46 -0500

    Implementation module Branch

---
+++ microservice-logistic/src/main/java/com/microservice/logistic/models/Branch.java
@@ -0,0 +1,69 @@
+package com.microservice.logistic.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "branch")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "direccion", nullable = false)
+    private String direccion;
+
+    @Column(name = "telefono", nullable = false)
+    private String telefono;
+
+    @Column(name = "encargado")
+    private String encargado;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEncargado() {
+		return encargado;
+	}
+
+	public void setEncargado(String encargado) {
+		this.encargado = encargado;
+	}
+}
