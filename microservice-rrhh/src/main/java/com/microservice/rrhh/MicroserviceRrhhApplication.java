a58a111731ef99528da5f4f70a5f5f294b895db1
Yamada-decs <u22100700@utp.edu.pe>
2025-06-01 22:29:55 -0500

    Gateway advance gaaaaaaaa

--- microservice-rrhh/src/main/java/com/microservice/rrhh/MicroserviceRrhhApplication.java
+++ microservice-rrhh/src/main/java/com/microservice/rrhh/MicroserviceRrhhApplication.java
@@ -3,7 +3,9 @@ package com.microservice.rrhh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;  // Correct import
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MicroserviceRrhhApplication {
