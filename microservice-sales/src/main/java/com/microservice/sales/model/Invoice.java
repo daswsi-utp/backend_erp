2f65046b4e6bdce68d686ec50b725bb0888f4efb
egusquiza31 <egusquiza.31@hotmail.com>
2025-06-25 17:29:00 -0500

    attributes of the invoice class

--- microservice-sales/src/main/java/com/microservice/sales/model/Invoice.java
+++ microservice-sales/src/main/java/com/microservice/sales/model/Invoice.java
@@ -1,5 +1,48 @@
 package com.microservice.sales.model;
 
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "invoice")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Invoice {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_invoice")
+    private Long id;
+
+    @Column(name = "issue_date")
+    private Timestamp issueDate;
+
+    @Column(name = "invoice_number", unique = true)
+    private String invoiceNumber;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name = "total_amount")
+    private Double totalAmount;
+
+    @Column(name = "subtotal")
+    private Double subtotal;
+
+    @Column(name = "tax")
+    private Double tax;
+
+    @Column(name = "discount")
+    private Double discount;
+
+    @OneToOne
+    @JoinColumn(name = "quote_id", referencedColumnName = "id_quotes")
+    @JsonBackReference
+    private quote quote;
 }
